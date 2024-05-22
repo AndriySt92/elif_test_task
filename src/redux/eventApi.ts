0b3e2dbf55abb4164171b9f011fe52ac8d631f79
AndriySt92 <andriyua-1992@mail.ru>
2024-05-22 19:15:32 +0300

    Change baseUrl

--- src/redux/eventApi.ts
+++ src/redux/eventApi.ts
@@ -5,7 +5,7 @@ import { RegisterData } from "../interfaces/participantInterfaces"
 export const eventApi = createApi({
   reducerPath: "event",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/api/event",
+    baseUrl: "https://eliftech-test-server.vercel.app/api/event",
   }),
 
   endpoints: builder => ({
