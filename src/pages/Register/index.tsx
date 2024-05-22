dfed47d4a4b0a5737b757f0f0b065b88c58397f1
AndriySt92 <andriyua-1992@mail.ru>
2024-05-22 18:08:34 +0300

    Add Register page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./style.css"
+import { Form } from "../../components"
+
+export const Register = () => {
+  return (
+    <div className="register">
+      <Form />
+    </div>
+  )
+}
